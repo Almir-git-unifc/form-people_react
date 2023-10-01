8b6b63f50c9b92209418a82e418f89025271d552
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2023-09-30 23:14:01 -0300

    feat(react): v0.02.000 - send structure form data 'cadastro'

--- src/components/formcomp.jsx
+++ src/components/formcomp.jsx
@@ -4,12 +4,84 @@ const formcomp = () => {
       <form action='#'>
         {/*  div form-header tem o título cadastre-se para entrar */}
         <div className='form-header'>
-          <h1>Cadastre-se</h1>
+          <div className='title'><h2>Cadastre-se</h2></div>
+
+          <div className='login-button'>
+            <button>
+              <a href='#'>Entrar</a>
+            </button>
+          </div>
         </div>
-        <div className='login-button'>
-          <button>
-            <a href='#'>Entrar</a>
-          </button>
+
+        <div className='input-group'>
+
+          <div className='input-box'>
+            <label htmlFor='firstname'>Primeiro Nome</label>
+            <input 
+               id="firstname" 
+               type="text" 
+               name="firstname" 
+               placeholder="Digite seu primeiro nome" 
+               required 
+            />
+          </div>
+
+          <div className='input-box'>
+            <label htmlFor='lastname'>Sobrenome</label>
+            <input 
+               id="lastname" 
+               type="text" 
+               name="lastname" 
+               placeholder="Digite seu sobrenome" 
+               required 
+            />
+          </div>
+
+          <div className='input-box'>
+            <label htmlFor='email'>Email</label>
+            <input 
+               id="email" 
+               type="email" 
+               name="email" 
+               placeholder="Digite seu email" 
+               required 
+            />
+          </div>
+
+          <div className='input-box'>
+            <label htmlFor='number'>Celular</label>
+            <input 
+               id="number" 
+               type="text" 
+               name="number" 
+               placeholder="(xx) xxxx-xxxx" 
+               required 
+            />
+          </div>
+
+          <div className='input-box'>
+            <label htmlFor='password'>Senha</label>
+            <input 
+               id="password" 
+               type="password" 
+               name="password" 
+               placeholder="Digite sua senha" 
+               required 
+            />
+          </div>
+
+          <div className='input-box'>
+            <label htmlFor='password'>Confirme sua Senha</label>
+            <input 
+               id="password" 
+               type="password" 
+               name="confirmPassword" 
+               placeholder="Digite sua senha" 
+               required 
+            />
+          </div>
+
+
         </div>
       </form>
     </div>
